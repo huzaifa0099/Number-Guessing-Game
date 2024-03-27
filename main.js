f432d32362b401ba22ba2961a4e4abdd5a9ee48a
huzaifa0099 <155224112+huzaifa0099@users.noreply.github.com>
2024-03-27 08:13:15 -0700

    update

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        message: "please guess a number between 1-6 :",
+        type: "number",
+        name: "userGuessedNumber",
+    }
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log(chalk.green("Congratulations! You Guessed a right number."));
+}
+else {
+    console.log(chalk.red("You Guessed a wrong number!"));
+}
